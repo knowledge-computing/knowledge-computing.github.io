10ffef6e8a85829c60d5ca707f37957a7dc8b8cb
YOO-uN-ee <jiyoonp0228@gmail.com>
2026-02-04 14:17:13 -0600

    update migrate files and pub structure

--- scripts/migrate_files.py
+++ scripts/migrate_files.py
@@ -1,9 +1,147 @@
 """
 Migrates two years ago data in dynamic bib to static bib
-Static bib is free to update
-
-Read date information from 
-static.bib
-dynamic.bib
-publications.bib
+    with consideration for override and delete
 """
+import re
+from typing import List
+
+from pathlib import Path
+from datetime import datetime, timezone
+
+import bibtexparser
+
+ROOT = Path(__file__).resolve().parents[1]
+STATIC = ROOT / "_data/pub/static.bib"
+DYNAMIC = ROOT / "_data/pub/dynamic.bib"
+OVERRIDE= ROOT / "_data/pub/override.bib"
+DELETE = ROOT / "_data/pub/delete.bib"
+# OUT = ROOT / "publications.bib"
+
+def still_current(static_year:int,
+                  current_year:int) -> bool:
+    """
+    Compare current year with static.bib file year
+    """
+    if static_year == current_year:
+        return True
+    
+    return False
+
+def load(path: Path):
+    if not path.exists():
+        return []
+    with path.open("r", encoding="utf-8") as f:
+        db = bibtexparser.load(f)
+    return db.entries
+
+def get_static_year(path: Path) -> int:
+    """
+    Read the comment header of the static file
+    Extract the year portion
+    """
+    with path.open("r", encoding="utf-8") as f:
+        list_lines = f.readlines()
+
+    line_date = list_lines[2]
+    m = re.search(r"on\s+(\d{4})-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z", line_date)
+    year = m.group(1) if m else None
+
+    return int(year)
+
+def write_to_bibtex(merged,
+                    file_path: Path,
+                    auto_message: List[str],) -> None:
+    """
+    Write dictionary to file_path with auto_message and date
+    """
+
+    db = bibtexparser.bibdatabase.BibDatabase()
+    db.entries = list(merged.values())
+
+    writer = bibtexparser.bwriter.BibTexWriter()
+    writer.indent = "  "
+    body = writer.write(db)
+
+    ts = datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z")
+    header = (
+        "".join(f"% {line}\n" for line in auto_message) + 
+        f"% Updated on {ts}\n\n"
+    )
+
+    file_path.write_text(header + body, encoding="utf-8")
+    print(f"Wrote {file_path} with {len(db.entries)} entries.")
+
+def main():
+    current_year = datetime.now(timezone.utc).year
+    static_year = get_static_year(STATIC)
+    static_year = 2025
+
+    if still_current(static_year, current_year):
+        print("No changes to make yet")
+        return
+
+    static_entries = load(STATIC)
+    dynamic_entries = load(DYNAMIC)
+    delete_entries = load(DELETE)
+    override_entries = load(OVERRIDE)
+
+    # Create delete list for those that are more than 2 years ago
+    list_delete = []
+    remain_delete = {}
+    for e in delete_entries:
+        if "ID" in e:
+            if e["year"] <= str(current_year - 2):
+                list_delete.append(e["ID"])
+                continue
+            remain_delete[e["ID"]] = e
+    write_to_bibtex(remain_delete, DELETE,
+                    ["Publications that should not be visible on the lab webpage",
+                     "PLEASE COPY AND PASTE IT FROM THE DYNAMIC.BIB FILE",
+                     "If changes need to made for files in static.bib DO DIRECTLY IN STATIC.BIB"])
+
+    merged = {}
+    for e in static_entries:
+        if "ID" in e:
+            merged[e["ID"]] = e
+
+    remain_override = {}
+    dict_override = {}
+    for e in override_entries:
+        if "ID" in e:
+            if e["year"] <= str(current_year - 2):
+                dict_override[e["ID"]] = e
+                continue
+            else:
+                remain_override[e["ID"]] = e
+    write_to_bibtex(remain_override, OVERRIDE,
+                    ["PLEASE use the same reference ID when overriding information"])
+
+
+    # Identify dynamic items that are over year
+    remain_dynamic = {}
+    for e in dynamic_entries:
+        if "ID" in e:
+            unique_id = e["ID"]
+
+            if unique_id in list_delete:
+                continue
+
+            if e["year"] <= str(current_year - 2):
+                try:    # Override information
+                    merged[unique_id] = dict_override[unique_id]
+                except:
+                    merged[unique_id] = e
+
+                continue
+            else:
+                remain_dynamic[unique_id] = e
+
+    write_to_bibtex(remain_dynamic, DYNAMIC,
+                    ["AUTO-GENERATED FILE. DO NOT EDIT."])
+    
+    write_to_bibtex(merged, STATIC,
+                    ["STATIC BIBS - FEEL FREE TO EDIT",
+                     "DO NOT CHANGE ANY OF THE COMMENTS"])
+
+if __name__ == "__main__":
+    main()
